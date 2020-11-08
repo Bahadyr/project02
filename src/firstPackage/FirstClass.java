c6f125fbae4ef6c5c44a7399675ab9662dbe5159
Bahadyr <73485287+Bahadyr@users.noreply.github.com>
2020-11-08 11:21:52 -0500

    First commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -9,6 +9,11 @@ public class FirstClass {
 		System.out.println("Hello !");
 		System.out.println("Second commit from local mater ");
 		System.out.println("Third commit from local master ");
+		System.out.print(a+22);
+		a=a*a;
+		System.out.print(a);
+		System.out.println("First commit from remote mater branch ");
+		
 		   
 	}
 
