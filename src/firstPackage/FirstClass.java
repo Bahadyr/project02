004dd69f9ea5ee10fbba79f31d2f739f233face1
Bahadyr <eymirovbahadyr@gmail.com>
2020-11-08 12:08:00 -0500

    Forth from local master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -16,6 +16,7 @@ public class FirstClass {
 		System.out.println("First commit from remote mater branch ");
 		System.out.println("Second commit from remote mater branch ");
 		System.out.println("Third commit from remote mater branch ");
+		System.out.println("Here is a merge conflict I create from local master");
 		   
 	}
 
