987bb0fe8804c83d9471877a6e82849850762183
Bahadyr <eymirovbahadyr@gmail.com>
2020-11-08 11:13:57 -0500

    third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -1,11 +1,15 @@
 package firstPackage;
 
 public class FirstClass {
+	
+ static int a=44;
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello !");
 		System.out.println("Second commit from local mater ");
+		System.out.println("Third commit from local master ");
+		   
 	}
 
 }
